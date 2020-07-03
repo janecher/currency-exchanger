e2805687a2b97025f887da67eea337a6ce3fa1ad
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-02 20:49:30 -0700

    create CurrentService class for api calls

---
+++ src/currency-service.js
@@ -0,0 +1,16 @@
+export class CurrencyService {
+  async getConvertionByCurrency(currency) {
+    try {
+      let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return false;
+    }
+  }
+}
